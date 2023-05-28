df5df9b62ed642b5fdae6bec31ac1ec3aaa69f5b
Mouhamed Doumbia <doumbiasoft@gmail.com>
2023-05-27 21:51:49 -0400

    add category ans tag function

--- app/main/routes.py
+++ app/main/routes.py
@@ -2,37 +2,64 @@ from app.main import bp
 from app.extensions import API_URL_BASE,headers,get_data,render_template
 from random import randrange,sample
 
-#Jollof Rice
-#african#meat_loaf
 @bp.route('/')
 def index():
     url = f"{API_URL_BASE}/recipes/list"
-    url_tags = f"{API_URL_BASE}/tags/list"
+
+    """get the maximum recipes"""
     data_recipes_random = get_data(url,headers=headers,params={"from":"0","size":"100"})
+    """get the maximum dessert recipes"""
     data_recipes_desert = get_data(url,headers=headers,params={"from":"0","size":"100","tags":"desserts"})
-    data_tags = get_data(url_tags,headers=headers)
-    data_recipes_vegetarian = get_data(url,headers=headers,params={"from":"0","size":"100","tags":"thanksgiving_vegetarian"})
+    """get the maximum vegetarian recipes"""
+    data_recipes_vegetarian = get_data(url,headers=headers,params={"from":"0","size":"100","tags":"vegetarian"})#frank_s_vegetarian_snacks#vegetarian#thanksgiving_vegetarian
+    """get the most popular recipes"""
     data_recipes_most_popular = get_data(url,headers=headers,params={"from":"0","size":"100","tags":"christmas"})
-    data_recipes_meat_lover = get_data(url,headers=headers,params={"from":"0","size":"100","tags":"one_top_app_meat"})
+    """get the maximum meat lover recipes"""
+    data_recipes_meat_lover = get_data(url,headers=headers,params={"from":"0","size":"100","tags":"one_top_app_meat"})#meat_loaf
+    """get the maximum gluten free recipes"""
     data_recipes_gluten_free = get_data(url,headers=headers,params={"from":"0","size":"100","tags":"gluten_free"})
-    data_recipes_feature = get_data(url,headers=headers,params={"from":"0","size":"100","tags":"tasty_holiday_festive_favorites"})
-    
-    tags = data_tags.results
+    """get the maximum african recipes"""
+    data_recipes_african = get_data(url,headers=headers,params={"from":"0","size":"100","tags":"african"}) #african#Jollof Rice
+
+
     random_number = randrange(0, len(data_recipes_random.results) - 1)
     random_recipe = data_recipes_random.results[random_number]
-    recipes_desert = sample(data_recipes_desert.results, 4)
     
-    recipes_vegetarian = sample(data_recipes_vegetarian.results, 4)
-    recipes_most_popular = sample(data_recipes_most_popular.results, 5)
-    recipes_meat_lover = sample(data_recipes_meat_lover.results, 5)
-    recipes_gluten_free = sample(data_recipes_gluten_free.results, 5)
-    recipes_feature = sample(data_recipes_feature.results, 2)
+    if len(data_recipes_desert.results)>4:
+        recipes_desert = sample(data_recipes_desert.results, 4)
+    else:
+        recipes_desert = sample(data_recipes_desert.results, len(data_recipes_desert.results))
+
+    if len(data_recipes_most_popular.results)>5:
+        recipes_most_popular = sample(data_recipes_most_popular.results, 5)
+    else:
+        recipes_most_popular = sample(data_recipes_most_popular.results, len(data_recipes_most_popular.results))
+
+    if len(data_recipes_meat_lover.results)>5:
+        recipes_meat_lover = sample(data_recipes_meat_lover.results, 5)
+    else:
+        recipes_meat_lover = sample(data_recipes_meat_lover.results, len(data_recipes_meat_lover.results))
+
+    if len(data_recipes_gluten_free.results)>5:
+        recipes_gluten_free = sample(data_recipes_gluten_free.results, 5)
+    else:
+        recipes_gluten_free = sample(data_recipes_gluten_free.results, len(data_recipes_gluten_free.results))
+
+    if len(data_recipes_vegetarian.results)>5:
+        recipes_vegetarian = sample(data_recipes_vegetarian.results, 5)
+    else:
+        recipes_vegetarian = sample(data_recipes_vegetarian.results, len(data_recipes_vegetarian.results))
+
+    if len(data_recipes_african.results)>2:
+        recipes_african = sample(data_recipes_african.results, 2)
+    else:
+        recipes_african = sample(data_recipes_african.results, len(data_recipes_african.results))
     #breakpoint()
     
     return render_template('index.html',random_recipe = random_recipe, recipes_desert = recipes_desert,
                            recipes_vegetarian=recipes_vegetarian,recipes_most_popular=recipes_most_popular,
                            recipes_meat_lover=recipes_meat_lover,recipes_gluten_free=recipes_gluten_free,
-                           recipes_feature=recipes_feature)
+                           recipes_african=recipes_african)
 
 @bp.route('/recipes/favorites')
 def recipes_favorites():
--- app/search/routes.py
+++ app/search/routes.py
@@ -1,13 +1,43 @@
-from app.extensions import render_template,redirect,flash,url_for,abort,request,session
+from app.extensions import render_template,redirect,flash,url_for,abort,request,session,API_URL_BASE,headers,get_data
 from app.search import bp
 from app.forms.search.recipes import SearchForm
+
+url_tags = f"{API_URL_BASE}/tags/list"
 
 @bp.route('/recipes', methods=['GET','POST'])
 def recipes():
     """Search view"""
-    return render_template('search/recipes.html')
+
+    """get all tags"""
+    url_tags = f"{API_URL_BASE}/tags/list"
+    data_tags = get_data(url_tags,headers=headers)
+    #tags = data_tags.results
+    return render_template('search/recipes.html',tags=get_tag_by_type("meal"),list_type=get_type())
 
 @bp.route('/recipes/details', methods=['GET','POST'])
 def recipes_details():
     """Detail recipe view"""
     return render_template('search/details.html')
+
+def get_type():
+    data_tags = get_data(url_tags,headers=headers)
+    tags = data_tags.results
+    seen = set()
+    unique_list = []
+    for obj in tags:
+        if obj.type not in seen:
+            unique_list.append({"id":f"{obj.type}","name":f"{obj.type.replace('_',' ')}"})
+            seen.add(obj.type)
+    return unique_list
+
+def get_tag_by_type(type:str):
+    data_tags = get_data(url_tags,headers=headers)
+    tags = data_tags.results
+    seen = set()
+    unique_list = []
+    for obj in tags:
+        if obj.type == type:
+            if obj.name not in seen:
+               unique_list.append({"id":f"{obj.name}","name":f"{obj.display_name.lower()}"})
+               seen.add(obj.name)
+    return unique_list
